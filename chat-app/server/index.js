789ff64c230a352511098a4593b3990b3baa9ac8
tusharTalan1 <tushartalan10@gmail.com>
2026-09-18 15:58:39 +0530

    attached authroutes and frontend

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -3,19 +3,43 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const cors = require('cors');
+const { Server } = require('socket.io');
 
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
+const socketHandler = require('./socket/socketHandler');
 
 const app = express();
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.use('/api/auth', authRoutes);
+
+app.get('/chat', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/chat.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+socketHandler(io);
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+

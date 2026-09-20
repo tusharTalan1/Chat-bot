71094f04bee5c564807b15d797d2686982d9ae52
tusharTalan1 <tushartalan10@gmail.com>
2026-09-20 15:37:59 +0530

    fixing error

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -39,7 +39,7 @@ socketHandler(io);
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
